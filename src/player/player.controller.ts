b1f5d0b08cce6a9475902e08c2df94540e549b1f
JoSeHD3 <sebastian.bozek3@gmail.com>
2026-01-20 21:55:47 +0100

    Player controller refactor - change on GET to params instead of query

--- src/player/player.controller.ts
+++ src/player/player.controller.ts
@@ -1,21 +1,20 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { PlayerService } from './player.service';
 import type { RiotRegion } from 'src/types';
 
-//TODO: refactor na @Param
 @Controller('player')
 export class PlayerController {
     constructor(private readonly playerService: PlayerService) {}
 
-    @Get('account') //as account we think of riot PUUID
-    getAccount(@Query('nick') nick: string, @Query('tag') tag: string) {
+    @Get('account/:nick/:tag') //as account we think of riot PUUID
+    getAccount(@Param('nick') nick: string, @Param('tag') tag: string) {
         return this.playerService.getAccount(nick, tag);
     }
 
-    @Get('summoner') //Player is a summoner
+    @Get('summoner/:region/:puuid') //Player is a summoner
     getSummoner(
-        @Query('region') region: RiotRegion,
-        @Query('puuid') puuid: string
+        @Param('region') region: RiotRegion,
+        @Param('puuid') puuid: string
     ) {
         return this.playerService.getSummoner(region, puuid);
     }

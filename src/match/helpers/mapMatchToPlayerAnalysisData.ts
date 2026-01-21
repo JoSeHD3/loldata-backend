ca5012fbf5a2683bacbb5443496422a2885ef771
JoSeHD3 <sebastian.bozek3@gmail.com>
2026-01-21 22:55:36 +0100

    Added match data mapper for a analysis ready-to-export object

---
+++ src/match/helpers/mapMatchToPlayerAnalysisData.ts
@@ -0,0 +1,32 @@
+import { Match } from '../interfaces/Match';
+import { PlayerAnalysisData } from '../interfaces/PlayerAnalysisData';
+
+export const mapMatchToPlayerAnalysisData = (
+    match: Match,
+    puuid: string
+): PlayerAnalysisData => {
+    const player = match.info.participants.find(
+        player => player.puuid === puuid
+    );
+
+    //TODO error handling
+    if (!player) throw new Error('Player not found in a match');
+
+    const minutes = match.info.gameDuration / 60;
+    const cs = player.totalMinionsKilled + player.neutralMinionsKilled;
+
+    return {
+        gameDuration: match.info.gameDuration,
+
+        goldPerMinute: player.goldEarned / minutes,
+        csPerMinute: cs / minutes,
+
+        kda: (player.kills + player.assists) / player.deaths,
+        damageToChampions: player.totalDamageDealtToChampions,
+
+        damageToObjectives: player.damageDealtToObjectives,
+        objectivesTaken: player.turretTakedowns + player.inhibitorTakedowns,
+
+        visionScore: player.visionScore,
+    };
+};
---
+++ src/match/interfaces/PlayerAnalysisData.ts
@@ -0,0 +1,14 @@
+export interface PlayerAnalysisData {
+    gameDuration: number;
+
+    goldPerMinute: number;
+    csPerMinute: number;
+
+    kda: number;
+    damageToChampions: number;
+
+    damageToObjectives: number;
+    objectivesTaken: number;
+
+    visionScore: number;
+}

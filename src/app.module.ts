f61f9c9bc64080c88e9f1554c6778091179eca7f
JoSeHD3 <sebastian.bozek3@gmail.com>
2026-02-10 20:05:20 +0100

    Connection to mongodb by mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { PlayerController } from './player/player.controller';
 import { PlayerService } from './player/player.service';
 import { HttpModule } from '@nestjs/axios';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MatchController } from './match/match.controller';
 import { MatchService } from './match/match.service';
 import { AnalysisController } from './analysis/analysis.controller';
@@ -10,9 +10,21 @@ import { AnalysisService } from './analysis/analysis.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-    imports: [HttpModule, ConfigModule.forRoot(), UsersModule],
+    imports: [
+        HttpModule,
+        ConfigModule.forRoot(),
+        UsersModule,
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (config: ConfigService) => ({
+                uri: config.get<string>('MONGO_URI'),
+            }),
+            inject: [ConfigService],
+        }),
+    ],
     controllers: [
         PlayerController,
         MatchController,

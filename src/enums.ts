0c0d220bf8ec51df478bb76922728756f6d49511
JoSeHD3 <sebastian.bozek3@gmail.com>
2026-02-17 14:39:03 +0100

    Added roles for users

--- src/enums.ts
+++ src/enums.ts
@@ -2,3 +2,8 @@ export enum MatchType {
     SOLO = 420,
     FLEX = 440,
 }
+
+export enum UserRole {
+    USER = 'user',
+    SUB = 'sub',
+}
--- src/users/user.schema.ts
+++ src/users/user.schema.ts
@@ -1,5 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { UserRole } from 'src/enums';
 
 @Schema({ timestamps: true })
 export class User extends Document {
@@ -8,6 +9,9 @@ export class User extends Document {
 
     @Prop({ required: true })
     password: string;
+
+    @Prop({ type: String, enum: UserRole, default: UserRole.USER })
+    role: UserRole;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
